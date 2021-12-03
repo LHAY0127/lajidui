814e6fc5bd1949e0885128c857812f76fdc09ca5
Joker <Joker@atguigu.com>
2021-12-04 01:21:43 +0800

    hot-fix first commit

--- git-test/src/main/java/git/GitTest.java
+++ git-test/src/main/java/git/GitTest.java
@@ -3,6 +3,6 @@ package git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
-        System.out.println("hello Git");
+        System.out.println("hello Git2");
     }
 }
